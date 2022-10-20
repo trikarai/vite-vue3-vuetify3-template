9a416360b643e90f967ed37ec1870ed2f862c927
Tri Sutrisno <mas.trisutrisno@gmail.com>
2022-10-20 12:14:44 +0700

    add eslint and prettier

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 import { fileURLToPath, URL } from "node:url";
 
-import vue from '@vitejs/plugin-vue'
+import vue from "@vitejs/plugin-vue";
+// import eslintPlugin from 'vite-plugin-eslint';
 
 // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
-import vuetify from 'vite-plugin-vuetify'
+import vuetify from "vite-plugin-vuetify";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		vue(),
-		vuetify({ autoImport: true }),
-	],
-	resolve: {
-		alias: {
-			"@": fileURLToPath(new URL("./src", import.meta.url)),
-		},
-	},
-	build: {
-		target: 'esnext'
-	}
-})
+  plugins: [vue(), vuetify({ autoImport: true })],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  build: {
+    target: "esnext",
+  },
+});
