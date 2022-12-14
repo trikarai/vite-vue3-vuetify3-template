672e5501cd3beeae08814c477dfefb029d647b7c
Tri Sutrisno <mas.trisutrisno@gmail.com>
2022-12-14 22:31:47 +0700

    @intlify/vite-plugin-vue-i18n

--- src/plugins/i18n/index.js
+++ src/plugins/i18n/index.js
@@ -4,10 +4,10 @@ import en from "@/locale/en.locale.js";
 import id from "@/locale/id.locale.js";
 
 const i18n = new createI18n({
-    legacy: false, // Vuetify does not support the legacy mode of vue-i18n
-    locale: "en",
-    fallbackLocale: "en",
-    messages: { en, id }
+  legacy: false, // Vuetify does not support the legacy mode of vue-i18n
+  locale: "id",
+  fallbackLocale: "en",
+  messages: { en, id },
 });
 
 export default i18n;
--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 import { fileURLToPath, URL } from "node:url";
-import path from 'path'
+import path from "path";
 
-import vue from '@vitejs/plugin-vue'
+import vue from "@vitejs/plugin-vue";
 // import eslintPlugin from 'vite-plugin-eslint';
 
 // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
-import vuetify from 'vite-plugin-vuetify'
-import vueI18n from '@intlify/vite-plugin-vue-i18n'
+import vuetify from "vite-plugin-vuetify";
+import VueI18nPlugin from "@intlify/unplugin-vue-i18n/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vuetify({ autoImport: true }),
-    vueI18n({
+    VueI18nPlugin({
       // if you want to use Vue I18n Legacy API, you need to set `compositionOnly: false`
       // compositionOnly: false,
-      include: path.resolve(__dirname, './src/plugins/i18n/lang/**')
-    })
+      include: path.resolve(__dirname, "./src/plugins/i18n/lang/**"),
+    }),
   ],
   resolve: {
     alias: {
@@ -26,6 +26,6 @@ export default defineConfig({
     },
   },
   build: {
-    target: 'esnext'
-  }
-})
+    target: "esnext",
+  },
+});
